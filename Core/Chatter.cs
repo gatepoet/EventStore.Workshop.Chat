2474f0f0b2cd5892a89763531e97ed0ec940b501
Kristoffer Rolf Deinoff <kristoffer.deinoff@gmail.com>
2014-11-23 14:55:12 +0200

    Extracted chatter into own assembly

---
+++ Core/Chatter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using EventStore.ClientAPI;
+using EventStore.ClientAPI.SystemData;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Core
+{
+    public class ChatMessage
+    {
+        public string User { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class Chatter
+    {
+        public static bool Connected;
+        public static string User;
+        public static string ChatRoom;
+        public static IEventStoreConnection Connection;
+        public static UserCredentials _userCredentials;
+        public static JsonSerializerSettings serializerSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+
+        public static void Main()
+        {
+            _userCredentials = new UserCredentials("admin", "changeit");
+            var settings = ConnectionSettings.Create()
+                .UseConsoleLogger()
+                .SetDefaultUserCredentials(_userCredentials)
+                .KeepReconnecting()
+                .KeepRetrying();
+
+            Connection = EventStoreConnection.Create(
+                settings,
+                new IPEndPoint(IPAddress.Parse("54.77.248.243"), 1113));
+
+            Connection.Connected += OnConnected;
+            Connection.ConnectAsync();
+            Connection.SubscribeToStreamAsync(
+                ChatRoom,
+                false,
+                ChatMessageRecieved);
+
+
+            System.Console.WriteLine("Connecting...");
+        }
+
+        private static void OnConnected(object sender, ClientConnectionEventArgs e)
+        {
+            Connected = true;
+            System.Console.WriteLine("Connected to " + ChatRoom);
+        }
+
+        public static void SendMessage(string text, string user)
+        {
+            var message = new ChatMessage
+            {
+                User = user,
+                Message = text
+            };
+
+            var json = JsonConvert.SerializeObject(message, serializerSettings);
+            var bytes = Encoding.UTF8.GetBytes(
+                json);
+            var eventData = new EventData(
+                Guid.NewGuid(),
+                "chatMessage",
+                true,
+                bytes,
+                new byte[0]);
+
+            Connection.AppendToStreamAsync(
+                ChatRoom,
+                ExpectedVersion.Any,
+                eventData);
+        }
+        private static Dictionary<string, bool> xxx = new Dictionary<string, bool>(); 
+        private static void ChatMessageRecieved(EventStoreSubscription sender, ResolvedEvent e)
+        {
+            var json = Encoding.UTF8.GetString(e.Event.Data);
+            var message = JsonConvert.DeserializeObject<ChatMessage>(json);
+
+            if (message.User == User)
+                return;
+
+            var text = string.Format(
+                "{0} says:\n{1}",
+                message.User,
+                message.Message);
+
+            System.Console.WriteLine(text);
+            if (!xxx.ContainsKey(message.User))
+            {
+                xxx.Add(message.User, false);
+            }
+            if (!xxx[message.User])
+            {
+                xxx[message.User] = true;
+                SendMessage(message.Message, message.User);
+                    Thread.Sleep(5000);
+                    xxx[message.User] = false;
+            }
+        }
+
+        class ChatMessage
+        {
+            public string User { get; set; }
+            public string Message { get; set; }
+        }
+
+    }
+}
--- EventStore.Chat.Console/Program.cs
+++ EventStore.Chat.Console/Program.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Core;
 using EventStore.ClientAPI;
 using EventStore.ClientAPI.SystemData;
 using Newtonsoft.Json;
@@ -16,46 +17,20 @@ namespace EventStore.Chat.Console
     class Program
     {
         private static bool Running = true;
-        private static bool Connected;
-        private static string User;
-        private static string ChatRoom;
-        private static IEventStoreConnection Connection;
-        private static UserCredentials _userCredentials;
-        private static JsonSerializerSettings serializerSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
-
         static void Main(string[] args)
         {
-            User = args[0];
-            ChatRoom = args[1];
-            _userCredentials = new UserCredentials("admin", "changeit");
-            var settings = ConnectionSettings.Create()
-                .UseConsoleLogger()
-                .SetDefaultUserCredentials(_userCredentials)
-                .KeepReconnecting()
-                .KeepRetrying();
-
-            Connection = EventStoreConnection.Create(
-                settings,
-                new IPEndPoint(IPAddress.Parse("54.77.248.243"), 1113));
-
-            Connection.Connected += OnConnected;
-            Connection.ConnectAsync();
-            Connection.SubscribeToStreamAsync(
-                ChatRoom,
-                false,
-                ChatMessageRecieved);
-
-
-            System.Console.WriteLine("Connecting...");
+            Chatter.User = args[0];
+            Chatter.ChatRoom = args[1];
+            Chatter.Main();
             while (Running)
             {
-                if (Connected)
+                if (Chatter.Connected)
                 {
                     var message = System.Console.ReadLine();
                     if (message == "!q")
                         Running = false;
                     else
-                        SendMessage(message);
+                        Chatter.SendMessage(message, Chatter.User);
                 }
                 else
                 {
@@ -64,56 +39,5 @@ namespace EventStore.Chat.Console
             }
         }
 
-        private static void OnConnected(object sender, ClientConnectionEventArgs e)
-        {
-            Connected = true;
-            System.Console.WriteLine("Connected to " + ChatRoom);
-        }
-
-        public static void SendMessage(string text)
-        {
-            var message = new ChatMessage
-            {
-                User = User,
-                Message = text
-            };
-
-            var json = JsonConvert.SerializeObject(message, serializerSettings);
-            var bytes = Encoding.UTF8.GetBytes(
-                json);
-            var eventData = new EventData(
-                Guid.NewGuid(),
-                "chatMessage",
-                true,
-                bytes,
-                new byte[0]);
-
-            Connection.AppendToStreamAsync(
-                ChatRoom,
-                ExpectedVersion.Any,
-                eventData);
-        }
-
-        private static void ChatMessageRecieved(EventStoreSubscription sender, ResolvedEvent e)
-        {
-            var json = Encoding.UTF8.GetString(e.Event.Data);
-            var message = JsonConvert.DeserializeObject<ChatMessage>(json);
-
-            if (message.User == User)
-                return;
-
-            var text = string.Format(
-                "{0} says:\n{1}",
-                message.User,
-                message.Message);
-
-            System.Console.WriteLine(text);
-        }
-
-        class ChatMessage
-        {
-            public string User { get; set; }
-            public string Message { get; set; }
-        }
     }
 }
